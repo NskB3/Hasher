1fbcee7bee7104db2bb72387d065006cdae680c8
NskB3 <40596952+NskB3@users.noreply.github.com>
2018-08-02 13:20:04 +0200

    Add files via upload

---
+++ hasherSHA256.py
@@ -0,0 +1,49 @@
+import hashlib
+import itertools
+import sys
+
+hash = input("Hash to crack: ").replace(' ', '')
+def dictionary():
+    wlist = input("Wordlist: ")
+    try:
+        f = open(wlist, 'r').readlines()
+    except IOError:
+        print("Wordlist not found!")
+        quit()
+    for guess in f:
+        newhash = hashlib.sha256(guess.encode("utf-8")).hexdigest()
+        if newhash == hash:
+            print("\nHash Cracked:", guess)
+        else:
+            sys.stdout.write("\rTesting " + guess)
+            sys.stdout.flush()
+def brute():
+    charset = input("Charset: ")
+    for length in range(1, 9999):
+        gen = itertools.product(charset, repeat=length)
+        for i in gen:
+            new = hashlib.sha256(''.join(i).encode("utf-8")).hexdigest()
+            if new == hash:
+                frst = ''.join(i).replace(',', '')
+                frstt = frst.replace('(', '')
+                frsttt = frstt.replace(')', '')
+                print("\nHash Cracked:", frsttt)
+                quit()
+            else:
+                frst = ''.join(i).replace(',', '')
+                frstt = frst.replace('(', '')
+                frsttt = frstt.replace(')', '')
+                sys.stdout.write("\rTesting %s " % str(frsttt))
+                sys.stdout.flush()
+print("""
+1. Dictionary Attack
+2. Bruteforce
+""")
+choose = input(">> ")
+if choose == "1":
+    dictionary()
+elif choose == "2":
+    brute()
+else:
+    print("Not a valid option")
+    quit()
